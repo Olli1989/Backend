af7adc5ef92a4129352f1e074339940ba10f79ba
Olli1989 <oliver.straberger@gmx.at>
2022-06-23 15:16:31 +0200

    adding login and signup routes

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const express = require('express')
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv'
 
-require('dotenv').config()
+import userRouter from './routes/user.js'
+
+
 const app = express()
-const PORT = process.env.PORT
+dotenv.config()
 
-app.get('/', (req,res) => {
-  res.send("Server is running")
-})
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
 
 
+app.use('/user', userRouter)
 
-app.listen(PORT, ()=> {
-  console.log("Server is running on Port " + PORT)
-})
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true })
+  .then(()=> app.listen(PORT, ()=> {
+    console.log("Server is running on Port " + PORT)
+  }))
+  .catch((error)=>console.log(error.message))
+
